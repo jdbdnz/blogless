6aec6b9e64a973acd871e97be3887cca91479225
Josh Dean <jdbdean@gmail.com>
2018-07-09 22:45:23 -0700

    change Date to show relative time

--- client/src/components/Date/Presenter.js
+++ client/src/components/Date/Presenter.js
@@ -1,9 +1,17 @@
 import React from "react";
 import Moment from "moment";
+import TimeAgo from "react-timeago";
 import "./style.css";
 
-const Presenter = ({ date }) => (
-  <div className="Date">{Moment(date).format("MMMM Do YYYY, h:mm:ss a")}</div>
-);
+const Presenter = ({ date }) => {
+  const localDate = Moment(date).local();
+  return (
+    <TimeAgo
+      date={localDate}
+      title={localDate.format("MMMM Do YYYY, h:mm:ss a")}
+      className="Date"
+    />
+  );
+};
 
 export default Presenter;
