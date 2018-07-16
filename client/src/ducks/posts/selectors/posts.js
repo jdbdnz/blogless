f34eec557125c72771e33bf82865f9583254913d
Josh Dean <jdbdean@gmail.com>
2018-07-16 14:52:44 +0200

    change posts selector to ignore casing and refactor for comprehension

--- client/src/ducks/posts/selectors/posts.js
+++ client/src/ducks/posts/selectors/posts.js
@@ -1,21 +1,31 @@
-const posts = ({ posts }) => {
-  const terms = posts
+const postIncludesTerm = ({ post, term }) => {
+  return ["title", "body"].some(attribute => {
+    return post
+      .get(attribute)
+      .toLowerCase()
+      .includes(term.toLowerCase());
+  });
+};
+
+const filterPosts = ({ posts, terms }) => {
+  return posts.filter(post => {
+    return (
+      terms.length ===
+      terms.filter(term => postIncludesTerm({ post, term })).length
+    );
+  });
+};
+
+export default state => {
+  const posts = state.posts.get("posts");
+  const terms = state.posts
     .get("filter")
     .split(" ")
     .filter(x => x);
+
   if (terms.length) {
-    const filteredPosts = posts.get("posts").filter(post => {
-      const matchingTerms = terms.filter(term => {
-        return (
-          post.get("title").includes(term) || post.get("body").includes(term)
-        );
-      });
-      return matchingTerms.length;
-    });
-    return filteredPosts.toJS();
+    return filterPosts({ posts, terms }).toJS();
   } else {
-    return posts.get("posts").toJS();
+    return posts.toJS();
   }
 };
-
-export default posts;
