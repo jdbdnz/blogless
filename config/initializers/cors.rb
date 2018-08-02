bdbb04bc11ec3895cf26ca9a6b315bf1de303ba4
Josh Dean <jdbdean@gmail.com>
2018-08-01 20:08:54 -0700

    origin is the same, so cors is not necessary

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,12 +14,3 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
-
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins ['localhost:8000']
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
-end
